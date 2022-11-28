06c70240b3b91074827404d0559cb02dd4d0f56c
YannGUIOT <guiot_yann@hotmail.fr>
2022-11-28 21:24:47 +0100

    add order table

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
 
     has_many :carts
+    has_many :orders
 
     validates :title, presence: true, length: {in: 3..100, message: "Title lenght must be between 3 and 100" }
     validates :description, presence: true, length: {in: 10..500, message: "Description lenght must be between 10 and 500" }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_193213) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_201840) do
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
     t.datetime "created_at", null: false
@@ -25,6 +25,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_193213) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
