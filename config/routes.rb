ba97b7f6115c1117c306cf3b61a6abc8957cee32
JBE29 <jeanbaptiste.emily@gmail.com>
2022-11-29 07:21:27 +0100

    Stripe connection Ok

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
 
   resources :items
 
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+  end
 
 end
